79907d05781f4dfc0a44277fbfc54b5a6bd0df2d
Meathill <meathill@gmail.com>
2021-04-03 09:09:49 +0800

    change: fit OBS
    deps: upgraded deps

--- src/app.vue
+++ src/app.vue
@@ -13,17 +13,30 @@
         strong 晚
         strong.mx-2 9:00
         | -
-        strong.ml-2 10:40
+        strong.ml-2 10:00
+
+  .alert.alert-success.mb-1(hidden)
+    h3 🎁 下播前有抽奖！🎁
+    p 抽奖方式：
+    p 从今天的弹幕中随机抽一位同学
+    p 抽奖时必须在直播间
+
   h2 今日内容
   ul
-    li 9:00-9:10 准备直播
-    li 9:10-9:50 录 Vue 全家桶 + Serverless 实战开发
-    li 9:50-10:30 录浏览器扩展开发共享主页实战
-    li 10:30-10:40 答疑
+    li 9:00-9:05 准备直播
+    li 9:05-9:20 聊聊前端架构
+    li 9:20-9:45 开发 BB 酱
+    li 9:45-10:00 答疑
 
-  h3.mt-auto 待回答问题
+  h4.mt-auto 问题待聊
   ul
-    li （静待提问）
+    li 肉大 顺便再谈谈 前端架构 前端有架构吗
+
+  h4.mt-auto 招聘啦
+  ul
+    li 🎉 北京 每日优鲜 高级前端
+      br
+      | 🎉 薪资股票看水平 16薪
 
   footer.mt-auto 有问题请随时提出，我会不定时回答
     br
